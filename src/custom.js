da7f0f64897b149ff860ee3ea9ba29492435161a
Elina <elina.prints@gmail.com>
2021-05-01 14:14:54 +0300

    Fromat code and minor fixes

---
+++ src/custom.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const mars = document.getElementById("planeetmarss");
+  const phobos = document.getElementById("kuuphobos");
+  const deimos = document.getElementById("kuudeimos");
+  AFRAME.registerComponent("animationhandler", {
+    init: function () {
+      document.getElementById("paus").addEventListener("click", () => {
+        //alert('clicked');
+        if (this.el.components["animation"].isPlaying) {
+          this.el.components["animation"].pause();
+        } else {
+          this.el.components["animation"].play();
+        }
+      });
+    },
+  });
+  document.getElementById("planeet").addEventListener("click", function () {
+    mars.setAttribute("visible", "true");
+    phobos.setAttribute("visible", "false");
+    deimos.setAttribute("visible", "false");
+  });
+  document.getElementById("kuu1").addEventListener("click", function () {
+    phobos.setAttribute("visible", "true");
+    mars.setAttribute("visible", "false");
+    deimos.setAttribute("visible", "false");
+  });
+  document.getElementById("kuu2").addEventListener("click", function () {
+    deimos.setAttribute("visible", "true");
+    phobos.setAttribute("visible", "false");
+    mars.setAttribute("visible", "false");
+  });
+});
